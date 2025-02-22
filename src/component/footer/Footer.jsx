c217d532aa447f2d96d580f3105599ac39f332bd
rajendrachoudhary1 <itsmerajuchoudhary@gmail.com>
2025-02-22 11:36:05 +0530

    updated

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -15,15 +15,18 @@ const Footer = () => {
             <p>Foundersclub</p>
             <p>Follow on</p>
             <div className='footer-icons'>
-                <a href="https://x.com/?mx=2" className='footer-icon' ><span><FontAwesomeIcon icon={faXTwitter} /></span> Twitter</a>
-                <a href="https://in.linkedin.com/" className='footer-icon'><span><FontAwesomeIcon icon={faLinkedin} /></span> LinkedIn</a>
+              <div> <a href="https://x.com/?mx=2" className='footer-icon' ><span><FontAwesomeIcon icon={faXTwitter} /></span><span className='f-icon-title'>Twitter</span> </a></div>  
+              <div><a href="https://in.linkedin.com/" className='footer-icon'><span><FontAwesomeIcon icon={faLinkedin} /></span> <span  className='f-icon-title'>Linkedin</span></a></div>   
             </div>
          </div>
 
          <div className='footer-right'>
             <h3>Download from</h3>
+             <div>
              <a href="https://play.google.com/store/games?device=windows"><img src={playstore_icon} alt="" /></a>
              <a href="https://www.apple.com/app-store/"><img src={applestore_icon} alt="" /></a>
+             </div>
+            
 
          </div>
     </div>
