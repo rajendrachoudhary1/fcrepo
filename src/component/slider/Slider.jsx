0dad984f0e794ca1d81fe823382d7bc0f23513de
rajendrachoudhary1 <itsmerajuchoudhary@gmail.com>
2025-02-28 11:55:26 +0530

    updated

--- src/component/slider/Slider.jsx
+++ src/component/slider/Slider.jsx
@@ -1,4 +1,3 @@
- 
 import React from "react";
 import feature_img from "../../assets/Group 558.png";
 import feature_img1 from "../../assets/Group2.png";
@@ -8,61 +7,86 @@ import small_img1 from "../../assets/c1.png";
 import small_img2 from "../../assets/c2.png";
 import small_img3 from "../../assets/c3.png";
 import small_img4 from "../../assets/c4.png";
-import Feature from '../features/Feature'
+import Feature from "../features/Feature";
 import "../features/Feature.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-// import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+const featureinfo = [
+  {
+    title: "Global Networking",
+    info: "Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.",
+    small_icon: small_img1,
+    img: feature_img,
+  },
+  {
+    title: "Seamless Applications",
+    info: "Easily apply for jobs and events or manage applications for roles in your startup—all from one intuitive dashboard.",
+    small_icon: small_img2,
+    img: feature_img1,
+  },
+  {
+    title: "Industry Highlights",
+    info: "Never miss out on industry happenings. From tech summits to business workshops, explore events that expand your knowledge and network.",
+    small_icon: small_img3,
+    img: feature_img2,
+  },
+  {
+    title: "Startup Showcase",
+    info: "Founders can spotlight their startups to attract collaborators, investors, and talent, building visibility within a targeted audience.",
+    small_icon: small_img4,
+    img: feature_img3,
+  },
+];
 
-const featureinfo =[
-    {
-      title :'Global Networking',
-      info : 'Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.',
-      small_icon : small_img1,
-      img : feature_img
-    },
-    {
-      title :'Seamless Applications',
-      info : 'Easily apply for jobs and events or manage applications for roles in your startup—all from one intuitive dashboard.',
-      small_icon : small_img2,
-      img : feature_img1
-    },
-    {
-      title :'Industry Highlights',
-      info : 'Never miss out on industry happenings. From tech summits to business workshops, explore events that expand your knowledge and network.',
-      small_icon : small_img3,
-      img : feature_img2
-    },
-    {
-      title :'Startup Showcase',
-      info : 'Founders can spotlight their startups to attract collaborators, investors, and talent, building visibility within a targeted audience.',
-      small_icon : small_img4,
-      img : feature_img3
-    },
-  ]
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1, 
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+    slidesToSlide: 1,  
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1,  
+  },
+};
 
-
-const Slider = () => {
+const Slider = (props) => {
   return (
-  
-     <Swiper
-          cssMode={true}
-          navigation={true}
-          pagination={true}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-          className="mySwiper"
-      >
-        <SwiperSlide><Feature title ={featureinfo[0].title} info = {featureinfo[0].info} small_icon={featureinfo[0].small_icon} img= {featureinfo[0].img}/></SwiperSlide>
-        <SwiperSlide> <Feature title ={featureinfo[1].title} info = {featureinfo[1].info} small_icon={featureinfo[1].small_icon} img= {featureinfo[1].img}/></SwiperSlide>
-        <SwiperSlide><Feature title ={featureinfo[2].title} info = {featureinfo[2].info} small_icon={featureinfo[2].small_icon} img= {featureinfo[2].img}/></SwiperSlide>
-        <SwiperSlide>  <Feature title ={featureinfo[3].title} info = {featureinfo[3].info} small_icon={featureinfo[3].small_icon} img= {featureinfo[3].img}/></SwiperSlide>
-       
-      </Swiper>
+    <Carousel
+      swipeable={true}
+      draggable={true}
+      showDots={true}
+      responsive={responsive}
+      ssr={true} // Server-side rendering
+      infinite={true}
+      autoPlay={props.deviceType !== "mobile"}
+      autoPlaySpeed={2000}
+      keyBoardControl={true}
+      customTransition="all .5s ease-in-out"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={["mobile"]}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {featureinfo.map((feature, index) => (
+        <div key={index}>
+          <Feature
+            title={feature.title}
+            info={feature.info}
+            small_icon={feature.small_icon}
+            img={feature.img}
+          />
+        </div>
+      ))}
+    </Carousel>
   );
 };
 
